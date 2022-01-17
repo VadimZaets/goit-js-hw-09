931f6798329a669982647b5067c68e3a139c2a87
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-01-17 21:37:27 +0200

    1 задача

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+const changeBgColor = color => {
+  color.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  changeBgColor(document.body);
+  timerId = setInterval(() => {
+    changeBgColor(document.body);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
